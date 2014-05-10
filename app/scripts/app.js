8a27393a0fe06643e288e4a7ccc723151e4b4e5c
Lucian Dipeso <ldipeso@gmail.com>
2014-05-09 20:16:19 -0700

    Moved env config to separate file.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,9 +3,7 @@
 angular.module('luciandipeso.wl', ['ngAnimate', 'ngCookies', 'ngResource', 'ngRoute', 'ngSanitize', 'ngTouch', 'mgcrea.ngStrap', 'chieffancypants.loadingBar', 'infinite-scroll', 'angularMoment', 'google-maps'])
 
   .constant('version', 'v0.1.0')
-  .constant('settings', {
-    apiBase: "http://localhost:8080"
-  })
+  .constant('settings', wl.settings)
 
   .config(function($locationProvider, $routeProvider) {
 
